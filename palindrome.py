bf3a82b0284efc0b5bd9efef324d1013f3e74d54
kiranreddy681 <113874933+kiranreddy681@users.noreply.github.com>
2022-09-20 10:09:21 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,8 +1,7 @@
-Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-= RESTART: C:/Users/kredd/AppData/Local/Programs/Python/Python310/palindrome.py
-The string is a palindrome.
->>> 
-===================================================== RESTART: C:/Users/kredd/AppData/Local/Programs/Python/Python310/palindrome.py ====================================================
-The string is a palindrome.
+my_str = 'aIbohPhoBiA'
+my_str = my_str.casefold()
+rev_str = reversed(my_str)
+if list(my_str) == list(rev_str):
+   print("The string is a palindrome.")
+else:
+   print("The string is not a palindrome.")
