361e4c09747072edec6a6ff4b2fb4dc2ac5804bd
kiranreddy681 <113874933+kiranreddy681@users.noreply.github.com>
2022-09-20 10:04:14 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,8 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/kredd/AppData/Local/Programs/Python/Python310/palindrome.py
+The string is a palindrome.
+>>> 
+===================================================== RESTART: C:/Users/kredd/AppData/Local/Programs/Python/Python310/palindrome.py ====================================================
+The string is a palindrome.
